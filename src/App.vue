414d4490301e39e17b2538a23ab60247e206645b
palashkumardas <info.palash27@gmail.com>
2023-08-13 10:57:10 +0600

    basic props

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import TeamMember from "./components/TeamMember.vue";
+
+</script>
+<template>
+   <section class="mx-auto container">
+        <section class="bg-white dark:bg-gray-900">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+                <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Team</h2>
+                    <p class="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Explore the whole collection of open-source web components and elements built with the utility classes from Tailwind</p>
+                </div>
+                <div class="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+                 <TeamMember name="Palash" designation="CEO & Full Stack Developer" avatar="//flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" about="This is a wider card with supporting text below as a natural lead-in to additional content."/> 
+                 <TeamMember name="Palash" designation="CEO & Full Stack Developer" avatar="//flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png" about="This is a wider card with supporting text below as a natural lead-in to additional content."/> 
+                 <TeamMember name="Palash" designation="CEO & Full Stack Developer" avatar="//flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" about="This is a wider card with supporting text below as a natural lead-in to additional content."/> 
+                 <TeamMember name="Palash" designation="CEO & Full Stack Developer" avatar="//flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png" about="This is a wider card with supporting text below as a natural lead-in to additional content."/> 
+
+                </div>
+            </div>
+        </section>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
